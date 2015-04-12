d347fb7343ce75f3d7a66b304578e163165de6ab
Luis Deleon <mauricio@clasificados.com>
2015-04-12 14:46:28 -0600

    setted mysql username and password

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'knolic',
+			'username'  => 'knolic',
+			'password'  => 'knolic',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
