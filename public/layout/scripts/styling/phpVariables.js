ed8ea112745c2ba0f1a661e15ad080b1173b6a33
Luis Deleon <mauricio@clasificados.com>
2015-07-03 19:20:35 -0600

    fixed variables

--- public/layout/scripts/styling/phpVariables.js
+++ public/layout/scripts/styling/phpVariables.js
@@ -4,7 +4,7 @@ $(function(){
 		console.log('contains');
 		$p = $(this);
 		var text = $p.html();
-		text = text.replace(/(\$[a-zA-Z_]?[a-zA-Z_0-9]*(\[.*\]))/g, '<span class="phpVariable">$1</span>');
+		text = text.replace(/(\$[a-zA-Z_]?[a-zA-Z_0-9]*(\[.*\])*)/g, '<span class="phpVariable">$1</span>');
 		console.log(text);
 		$p.html(text);
 
